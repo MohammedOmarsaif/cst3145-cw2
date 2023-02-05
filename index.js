478d891d6212b558220c5bf4f32b910484fe2abf
Umar Saif <Mohdomarsaif@gmail.com>
2023-02-05 09:47:12 +0000

    Adding server file to AWS

--- index.js
+++ index.js
@@ -14,8 +14,24 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("short"));
 
+app.route("/api/:collection/:names/:options").get(
+    async function(req,res){
+        const {collection, names, options} = req.params;
 
+        if(!collection){res.send("NOT VALID COLLECTIONS or KEY")}
+    }
+).post(
+
+).put(
+
+).delete(
+
+);
+
+
+
+const url = process.env.URL || "http://localhost:";
 const port = process.env.PORT || 3000;
 app.listen(port, function(){
-    console.log("App is started on :"+port);
+    console.log("App is started on "+ url + port);
 });
